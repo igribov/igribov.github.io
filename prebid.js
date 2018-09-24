ab9df38752145f2b70b83fd57f2c0801d24f8cef
igribov <igribov@condenast.ru>
2018-09-24 14:47:19 +0300

    update

--- prebid.js
+++ prebid.js
@@ -5862,8 +5862,8 @@ pbjs.renderAd = function (doc, id) {
 
         if (renderer && renderer.url) {
           renderer.render(bid);
-          console.log('>>>>>>', utils.inIframe(), mediaType);
         } else if (doc === document && !utils.inIframe() || mediaType === 'video') {
+          console.log('>>>>>>', utils.inIframe(), mediaType);
           var message = 'Error trying to write ad. Ad render call ad id ' + id + ' was prevented from writing to the main document.';
           emitAdRenderFail(PREVENT_WRITING_ON_MAIN_DOCUMENT, message, bid);
         } else if (ad) {
